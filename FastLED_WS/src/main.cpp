9ca3599b98bf117a885eb7df22a85fa946f11465
Dimitre <dimitre@users.noreply.github.com>
2024-08-13 19:44:15 -0300

    up

--- FastLED_WS/src/main.cpp
+++ FastLED_WS/src/main.cpp
@@ -11,10 +11,14 @@ void setup() {
 void loopSeno() {
 	float maxValue = 28000.0f;
 	for (int a=0; a<3; a++) {
-		float r = std::sin(millis() * 0.002f  + a * 0.5f) * maxValue + maxValue;
-		float g = std::sin(millis() * 0.0015f + a * 0.5f) * maxValue + maxValue;
-		float b = std::sin(millis() * 0.001f  + a * 0.5f) * maxValue + maxValue;
-		float w = std::sin(millis() * 0.0005f + a * 0.5f) * maxValue + maxValue;
+		// float r = std::sin(millis() * 0.002f  + a * 0.5f) * maxValue + maxValue;
+		// float g = std::sin(millis() * 0.0015f + a * 0.5f) * maxValue + maxValue;
+		// float b = std::sin(millis() * 0.001f  + a * 0.5f) * maxValue + maxValue;
+		// float w = std::sin(millis() * 0.0005f + a * 0.5f) * maxValue + maxValue;
+		float r = sin(millis() * 0.002f  + a * 0.5f) * maxValue + maxValue;
+		float g = sin(millis() * 0.0015f + a * 0.5f) * maxValue + maxValue;
+		float b = sin(millis() * 0.001f  + a * 0.5f) * maxValue + maxValue;
+		float w = sin(millis() * 0.0005f + a * 0.5f) * maxValue + maxValue;
 		uint16_t red16 = static_cast<uint16_t>(r);
 		uint8_t r1 = red16 >> 8;
 		uint8_t r2 = red16 & 0xFF;
